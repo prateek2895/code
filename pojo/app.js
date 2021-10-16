d70d70540b59ebec81d07a38a4b2aefe55e162e0
root <root@ip-172-31-32-218.ap-south-1.compute.internal>
2021-10-16 14:36:37 +0000

    first code commit

---
+++ pojo/app.js
@@ -0,0 +1,15 @@
+var express= require("express");
+var app=express();
+var url = require("url");
+app.set('view engine', 'ejs');
+
+
+app.get("/*", function(req, res){
+    var pathname = url.parse(req.url).pathname;
+    console.log(pathname)
+    name1 = pathname.substring(1)
+    res.send("<h1>Hello "+ name1 + "</h1>")
+}).listen(80);
+
+
+console.log("Server Started at localhost:80");
